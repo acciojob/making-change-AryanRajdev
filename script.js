3882025e2251832a42be24152db39de09e3992af
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-12 19:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const makeChange = (c) => {
   // your name here
+	let cq = 0;
+	let cd = 0;
+	let cn = 0;
+	let cp = 0;
+
+	cq = Math.floor(c/25);
+	c = c%25;
+
+	cd = Math.floor(c/10);
+	c = c%10;
+
+	cq = Math.floor(c/5);
+	c = c%5;
+
+	cq = Math.floor(c/1);
+	c = c%1;
+
+	let ans = new Object();
+	ans.q = cq;
+	ans.d = cd;
+	ans.n = cn;
+	ans.p = cp;
+
+	return ans;
 };
 
-// Do not the change the code below
+Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
